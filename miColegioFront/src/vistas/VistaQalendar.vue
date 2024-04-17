ebb66d636cf937a85c55ef6051022741f01663a0
imunnic <imunnic1@gmail.com>
2024-04-17 14:01:02 +0200

    conseguido actualizar eventos

--- miColegioFront/src/vistas/VistaQalendar.vue
+++ miColegioFront/src/vistas/VistaQalendar.vue
@@ -1,44 +1,58 @@
 <template>
     <div>
-        <Qalendar
-            :events="eventos"
-        ></Qalendar>
+        <Qalendar :events="eventos" :config="configuracion" :key="dispo"></Qalendar>
         <v-btn @click="add"></v-btn>
     </div>
 </template>
 <script>
 import { Qalendar } from 'qalendar';
 export default {
-    components:{Qalendar},
+    components: { Qalendar },
     data() {
         return {
-            eventos:[
-                {
-          title: "Meeting with Dora",
-          with: "Albert Einstein",
-          time: { start: "2024-04-15 09:52", end: "2024-04-15 10:37" },
-          color: "green",
-          isEditable: true,
-          id: "de471c78cb5c",
-          description:
-            "Think of me as Yoda. Only instead of being little and green, I wear suites and I'm awesome.",
-        }]
-        }
-    },
-    methods:{
-        add(){
-            let evento = {
-          title: "Meeting with Dora",
-          with: "Albert Einstein",
-          time: { start: "2024-04-15 13:52", end: "2024-04-15 14:37" },
-          color: "green",
-          isEditable: true,
-          id: "de471c78cb5z",
-          description:
-            "Think of me as Yoda. Only instead of being little and green, I wear suites and I'm awesome.",
-        };
-        this.eventos.push(evento)
+            configuracion: {
+                dayBoundaries: {
+                    start: 9,
+                    end: 15
+                },
+                dayIntervals: {
+                    displayClickableInterval: true,
+                    height: 90
+                },
+                style: {
+                    fontFamily: 'arial'
+                }
+            },
+                eventos: [
+                    {
+                        title: "Meeting with Dora",
+                        with: "Albert Einstein",
+                        time: { start: "2024-04-15 09:52", end: "2024-04-15 10:37" },
+                        color: "green",
+                        isEditable: true,
+                        id: "de471c78cb5c",
+                        description:
+                            "Think of me as Yoda. Only instead of being little and green, I wear suites and I'm awesome.",
+                    }],
+                dispo:true
+            }
+        },
+            methods: {
+            add(){
+                let evento = {
+                    title: "Meeting with Dora",
+                    with: "Albert Einstein",
+                    time: { start: "2024-04-15 13:52", end: "2024-04-15 14:37" },
+                    color: "green",
+                    isEditable: true,
+                    id: "de471c78cb5z",
+                    description:
+                        "Think of me as Yoda. Only instead of being little and green, I wear suites and I'm awesome.",
+                };
+                this.eventos.push(evento)
+                console.log(this.eventos);
+                this.dispo=!this.dispo;
+            }
         }
     }
-}
 </script>
