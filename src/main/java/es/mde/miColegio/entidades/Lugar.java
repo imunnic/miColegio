e003cdbe2bacd14a20c558aa87dfc9bcf293e238
imunnic <imunnic1@gmail.com>
2024-05-27 12:19:18 +0200

    persistir lugares Fix #42

---
+++ src/main/java/es/mde/miColegio/entidades/Lugar.java
@@ -0,0 +1,44 @@
+package es.mde.miColegio.entidades;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="LUGARES")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="TIPO",
+    discriminatorType = DiscriminatorType.STRING)
+public class Lugar {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String nombre;
+  private int capacidad;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public int getCapacidad() {
+    return capacidad;
+  }
+
+  public void setCapacidad(int capacidad) {
+    this.capacidad = capacidad;
+  }
+
+  public Lugar() {
+  }
+
+}
